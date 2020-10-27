47d477cf3eaad56be07034d6deabfddc628abf20
Refayat Haque <rhaque@pyramidsystems.com>
2020-10-27 01:00:02 -0400

    Oct 27 0059

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <div class="block" :class="{animate: animatedBlock}"></div>
     <button @click="animateBlock">Animate</button>
   </div>
-  <base-modal @close="hideDialog" v-if="dialogIsVisible">
+  <base-modal @close="hideDialog" :open="dialogIsVisible">
     <p>This is a test dialog!</p>
     <button @click="hideDialog">Close it!</button>
   </base-modal>
@@ -11,10 +11,15 @@
     <button @click="showDialog">Show Dialog</button>
   </div>
   <div class="container">
-    <transition>
+    <transition name="para">
       <p v-if="paraIsVisible">This is only sometimes visible...</p>
     </transition>
     <button @click="toggleParagraph">Toggle Paragraph</button>
+  </div>
+  <div class="container">
+    <transition enter-to-class="" enter-active-class="" enter-class="">
+      <p>test</p>
+    </transition>
   </div>
 </template>  
 
@@ -82,8 +87,8 @@ button:active {
 .animate {
   /* transform: translateX(-150px); */
   /* ^ combined with transition CSS property in `block` class below */
-  animation: slide-fade 1s ease-out forwards;
-  /* with ^ syntax you just reference animation properties defined in `@keyframes`, preferred over what's above with `.block` */
+  animation: slide-scale 1s ease-out forwards;
+  /* with ^ pattern you just reference animation properties defined in `@keyframes`, preferred over what's above and its corresponding transition property within `.block` class below */
 }
 .block {
   width: 8rem;
@@ -93,7 +98,7 @@ button:active {
   /* transition: transform 1s ease-out; */
   /* ^ creates a smooth flow of the purple box to the left */
 }
-@keyframes slide-fade {
+@keyframes slide-scale {
   0% {
     transform: translateX(0) scale(1)
   }
@@ -104,25 +109,38 @@ button:active {
     transform: translateX(-150px) scale(1)
   }
 }
-.v-enter-from {
-   opacity: 0;
-   transform: translateY(-30px);
+/* prefixes of these vue classes need not be e.g., `v-enter-from`, 'v' can be supplanted with a unique prefix that corresponds to the `name` attribute in the <transition></transition> tag */
+/* if you have completely custom classes then you need to use special <transition></transition> tag attributes, look at line 20 (https://v3.vuejs.org/guide/transitions-enterleave.html#custom-transition-classes) */
+.para-enter-from {
+  /* opacity: 0;
+  transform: translateY(-30px); */
 }
-.v-enter-active {
-  transition: all 0.5s ease-out
+.para-enter-active {
+  animation: drops-down-with-blur 1s ease-out;
+  /* similar to whats explained on line 85, but we don't need `forwards` here for it to stay at the end state, you don't also need the vue classes above and below when using this pattern because the 'to' and 'from' stages of the animation are captured in the `@keyframes` */
 }
-.v-enter-to {
-  opacity: 1;
-  transform: translateY(0);
+.para-enter-to {
+  /* opacity: 1;
+  transform: translateY(0); */
 }
-.v-leave-from {
+@keyframes drops-down-with-blur {
+  0% {
+    opacity: 0;
+    transform: translateY(-30px);
+  }
+  100% {
+    opacity: 1;
+    transform: translateY(0);
+  }
+}
+.para-leave-from {
    opacity: 1;
    transform: translateY(0);
 }
-.v-leave-active {
+.para-leave-active {
   transition: all 0.5s ease-in
 }
-.v-leave-to {
+.para-leave-to {
   opacity: 0;
   transform: translateY(30px);
 }
